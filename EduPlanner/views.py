333cd1c7dc7da3b04d01e6ad12ac2d74a9b84c5e
avegas123 <avegas@usm.cl>
2024-11-19 15:12:39 -0300

    a

--- EduPlanner/views.py
+++ EduPlanner/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+
+def Feriados(request):
+    titulo = "Feriados"
+
+    info = requests.get("https://apis.digital.gob.cl/fl/feriados/2024")
+    feriados = info.json()
+
+    lista_feriados = list()
+
+    for p in feriados:
+        feriados = Feriados(
+        nombre = p["nombre"],
+        fecha = p["fecha"],
+        tipo = p["tipo"]
+        )
+
+        lista_feriados.append(feriados)
+
+    
+    data = {
+        "titulo":titulo,
+        "feriados":lista_feriados,
+    }
+
+    return render(request,'C3_Eduplanner/calendario.html',data)
